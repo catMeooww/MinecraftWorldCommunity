67c3df1e39b284f27821a12dc21b69fbbe3d6abf
Cat Meow <110690475+catMeooww@users.noreply.github.com>
2023-07-13 16:02:13 -0300

    Add files via upload

--- rooms.js
+++ rooms.js
@@ -1,4 +1,16 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyB2kMCF_wglD3NPBDjSl6phkYrorRI1LgE",
+  authDomain: "minecraft-world-community.firebaseapp.com",
+  databaseURL: "https://minecraft-world-community-default-rtdb.firebaseio.com",
+  projectId: "minecraft-world-community",
+  storageBucket: "minecraft-world-community.appspot.com",
+  messagingSenderId: "79164899563",
+  appId: "1:79164899563:web:36388146fef46073c63eae"
+};
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 //load user
 gamemode = "survival"
 
